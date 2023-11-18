db9fe8a2d3489a2329742ce36fba37faa8c4ed8a
Gabriel Torres <gaabrieltt7@gmail.com>
2023-11-17 23:36:47 -0300

    feat: delete product use case and controller

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import ClientRoute from '@/http/controllers/client/routes'
+import ProductRoute from '@/http/controllers/product/routes'
 import UserRoute from '@/http/controllers/user/routes'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import { InvalidCredentialsError } from './use-cases/errors/invalid-credentials-error'
+import { ProductAlreadyExistsError } from './use-cases/errors/product-already-exists-error'
 import { ResourceNotFoundError } from './use-cases/errors/resource-not-found-error'
 import { UserAlreadyAssociatedError } from './use-cases/errors/user-already-associated-error'
 import { UserAlreadyExistsError } from './use-cases/errors/user-already-exists-error'
@@ -14,6 +16,7 @@ app.use(express.json())
 
 app.use('/users', UserRoute)
 app.use('/clients', ClientRoute)
+app.use('/products', ProductRoute)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   let statusCode = 500
@@ -21,7 +24,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
   if (
     err instanceof UserAlreadyExistsError ||
-    err instanceof UserAlreadyAssociatedError
+    err instanceof UserAlreadyAssociatedError ||
+    err instanceof ProductAlreadyExistsError
   ) {
     statusCode = 409
   } else if (err instanceof InvalidCredentialsError) {
